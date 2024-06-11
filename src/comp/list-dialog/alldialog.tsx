ffa3f32fc7eeeda46ff11bbe60f19e146aabbd55
GHOSTxxx123 <negmatovazam4@mail.ru>
2024-06-11 12:14:06 +0600

    dev

--- src/comp/list-dialog/alldialog.tsx
+++ src/comp/list-dialog/alldialog.tsx
@@ -29,25 +29,39 @@ export const AllDialog: FC<{}> = () => {
 
     return (
         <div className="row-start-1 row-end-4 col-span-5 mt-10 mb-10 mr-10 rounded-md w-2/3 hover:shadow-2xl hover:shadow-purple-300 bg-white duration-300">
-            <div className="flex flex-col justify-center pt-20 mt-3 mb-20">
-                <h1 className="font-bold text-4xl text-center">История совещаний</h1>
-                <table className=" mt-10 ">
-                    <thead>
+             <div className="flex flex-col justify-center pt-20 mt-3 mb-20">
+                 <h1 className="font-bold text-4xl text-center mb-10">История совещаний</h1>
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-center text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th>Имя допроса</th>
-                            <th>Дата создания</th>
-                            <th>Дата модифик</th>
+                            <th scope="col" className="px-6 py-3">
+                                Имя допроса
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Дата создания
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Дата модифик
+                            </th>
+                            
                         </tr>
                     </thead>
-                    <tbody className="text-center bg-gray-300 rounded-md ">
+                    <tbody>
                         {data.map((item, index) => (
                             
-                            <tr key={index}>
+                            <tr key={index} className="bg-white border-b text-center hover:bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                                 <a href={`/dialog/${item.id}`}>
-                                <td>{item.name}</td>
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {item.name}
+                                    </th>
                                 </a>
-                                <td>{new Date(item.created_date).toLocaleDateString()}</td>
-                                <td>{new Date(item.last_modified).toLocaleDateString()}</td>
+                                <td className="px-6 py-4">
+                                    {new Date(item.created_date).toLocaleDateString()}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {new Date(item.last_modified).toLocaleDateString()}
+                                </td>
                             </tr>
                         ))}
                         {data.length === 0 && (
@@ -58,6 +72,40 @@ export const AllDialog: FC<{}> = () => {
                     </tbody>
                 </table>
             </div>
-        </div>
+
+            </div>
+            </div>
+
+        // <div className="row-start-1 row-end-4 col-span-5 mt-10 mb-10 mr-10 rounded-md w-2/3 hover:shadow-2xl hover:shadow-purple-300 bg-white duration-300">
+        //     <div className="flex flex-col justify-center pt-20 mt-3 mb-20">
+        //         <h1 className="font-bold text-4xl text-center">История совещаний</h1>
+        //         <table className=" mt-10 ">
+        //             <thead>
+        //                 <tr>
+        //                     <th>Имя допроса</th>
+        //                     <th>Дата создания</th>
+        //                     <th>Дата модифик</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody className="text-center bg-gray-300 rounded-md ">
+        //                 {data.map((item, index) => (
+                            
+        //                     <tr key={index}>
+        //                         <a href={`/dialog/${item.id}`}>
+        //                         <td>{item.name}</td>
+        //                         </a>
+        //                         <td>{new Date(item.created_date).toLocaleDateString()}</td>
+        //                         <td>{new Date(item.last_modified).toLocaleDateString()}</td>
+        //                     </tr>
+        //                 ))}
+        //                 {data.length === 0 && (
+        //                     <tr>
+        //                         <td colSpan={3}>No data available</td>
+        //                     </tr>
+        //                 )}
+        //             </tbody>
+        //         </table>
+        //     </div>
+        // </div>
     );
 };
